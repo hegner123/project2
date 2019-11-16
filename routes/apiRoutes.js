6a908c82613684dfb47c9504c9e721823c66d02e
Michael Hegner <hegner123@gmail.com>
2019-11-16 14:37:51 -0500

    merged conflicts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,14 +2,6 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-<<<<<<< HEAD
-=======
-  // app.get("/api/users", function(req, res) {
-  //   db.User.findAll({}).then(function(db) {
-  //     res.json(dbProject2);
-  //   });
-  // });
->>>>>>> 5b178c655668d0d5960633f0c3a2a3d2adec8c51
 
   // Create a new example
   app.post("/api/users", function(req, res) {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,14 +15,8 @@ module.exports = function(app) {
 app.get("/login", function(req, res) {
   res.render("login");
 });
-
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 5b178c655668d0d5960633f0c3a2a3d2adec8c51
+ 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
   });
 };
