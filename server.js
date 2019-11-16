99d90151e1dd4c88e5905bd4f259ae5cc22d6d3c
Michael Hegner <hegner123@gmail.com>
2019-11-16 11:02:23 -0500

    made updates to files to incorperate ejs engine

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
+var bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
 
 var db = require("./models");
 
@@ -12,14 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
@@ -33,15 +26,30 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
+// Express Engine
+var pageController = require('./routes/htmlRoutes');
+
+var app = express();s
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+  app.use(express.static(path.join(__dirname, '/')));
+  app.use(bodyParser.json());
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+  pageController(app);
+
 });
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
