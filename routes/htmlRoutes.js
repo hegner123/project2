1fcf851eb93cb258df27e16a70e0ff8c46e2e5e3
Michael Hegner <hegner123@gmail.com>
2019-11-16 12:20:09 -0500

    merge from pull

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,6 +8,10 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/create-account", function(req, res) {
+    res.render("create_account");
+});
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
