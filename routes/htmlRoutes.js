10bd8a57cf620e4abb3590044bd767d5711cde42
Michael Hegner <hegner123@gmail.com>
2019-11-16 13:22:28 -0500

    made deletions to api-routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,24 +3,14 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-      res.render("index", {
-        msg: "Welcome!"
-    });
+      res.render("index");
   });
 
   app.get("/create-account", function(req, res) {
     res.render("create_account");
 });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-
+  
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
