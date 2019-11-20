143cbabf36aa00caeef4c102c4b43e66aac80594
Michael Hegner <hegner123@gmail.com>
2019-11-20 16:50:51 -0500

    added user functionality. added create account. added user login. added inccorrect password response

---
+++ models/user.js
@@ -0,0 +1,72 @@
+
+
+var bcrypt = require("bcryptjs");
+//
+// Creating our User model
+//Set it as export because we will need it required on the server
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    // The email cannot be null, and must be a proper email before creation
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          notEmpty: true
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          notEmpty: true
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    zip: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  });
+  // Creating a custom method for our User model.
+  //This will check if an unhashed password entered by the
+  //user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
+  User.beforeCreate(user => {
+    user.password = bcrypt.hashSync(
+      user.password,
+       bcrypt.genSaltSync(10),
+       null
+     );
+   });
+  return User;
+};
+//This is a fix by Samaila Philemon Bala in case you want to use ES6
+//and the above is not working
+
