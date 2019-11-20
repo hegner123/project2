6f2fc0eaa61cffc6426a31b5c477a88f64dbc866
brownmj93 <brownmj93@gmail.com>
2019-11-19 19:42:29 -0500

    added books table and datasets

--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,16 @@ DROP IF EXISTS project2_db;
 CREATE DATABASE project2_db;
 
 USE project2_db;
+
+create table books (
+	book_id int,
+    isbn bigint (20),
+    authors text ,
+    original_publish_year int,
+    title text,
+    qty_on_hand int,
+    book_rating double,
+    image_url text,
+    small_image_url text,
+    primary key (book_id)
+);
