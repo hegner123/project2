f4abe0dafc0b9d2d853121b05bc1946c849e697c
Michael Hegner <hegner123@gmail.com>
2019-11-19 19:39:21 -0500

    merge

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,49 +1,52 @@
+//we import passport packages required for authentication
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+//
+//We will need the models folder to check passport agains
 var db = require("../models");
-
-module.exports = function(app) {
-
-  // Create a new user
-  app.post("/api/users", function(req, res) {
-    db.Users.create(req.body).then(function(dbUser){
-      res.json(dbUser);
+//
+// Telling passport we want to use a Local Strategy. In other words,
+//we want login with a username/email and password
+passport.use(new LocalStrategy(
+  // Our user will sign in using an email, rather than a "username"
+  {
+    usernameField: "email"
+  },
+  function(email, password, done) {
+    // When a user tries to sign in this code runs
+    db.User.findOne({
+      where: {
+        email: email
+      }
+    }).then(function(dbUser) {
+      // If there's no user with the given email
+      if (!dbUser) {
+        return done(null, false, {
+          message: "Incorrect email."
+        });
+      }
+      // If there is a user with the given email, but the password the user gives us is incorrect
+      else if (!dbUser.validPassword(password)) {
+        return done(null, false, {
+          message: "Incorrect password."
+        });
+      }
+      // If none of the above, return the user
+      return done(null, dbUser);
     });
-  });
-
-  // User Login
-  app.post('/api/auth', function(req, res, next) {
-    var loginEmail = req.body.email;
-    var loginPassword = req.body.userPassword;
-    if (loginEmail && loginPassword) {
-    db.Users.findOne({where : {email: loginEmail, userPassword: loginPassword}}).then(function(response, error) {
-      if (response.dataValues.email === loginEmail && response.dataValues.userPassword === loginPassword){
-
-          req.session.loggedin = true;
-          req.session.username = loginEmail;
-        console.log(req.session.loggedin);
-        res.redirect('../login');
-        } else {
-          res.send('Incorrect Username and/or Password!');
-        }
-        res.end();
-      });
-    } else {
-      res.send('Please enter Username and Password!');
-      res.end();
-    }
-  });
-
-  app.get("/api/login", function(req, res) {
-    res.send("Logged In")
-  });
-
-
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-
-}
+  }
+));
+//
+// In order to help keep authentication state across HTTP requests,
+// Sequelize needs to serialize and deserialize the user
+// Just consider this part boilerplate needed to make it all work
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+//
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+//
+// Exporting our configured passport
+module.exports = passport;
