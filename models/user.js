3fa2466775c98c7a63ea288103244bf0a656495f
Michael Hegner <hegner123@gmail.com>
2019-11-25 09:52:37 -0500

    trouble shooting heroku, made changes to user.js

--- models/user.js
+++ models/user.js
@@ -50,16 +50,7 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
 
-  User.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
-    User.belongsToMany(models.Book, {
-      through: "Checkout",
-      as: "User",
-      foreignKey: "userId",
-      otherKey: "bookId"
-    });
-  };
+
 
   // Creating a custom method for our User model.
   //This will check if an unhashed password entered by the
@@ -75,6 +66,17 @@ module.exports = function(sequelize, DataTypes) {
        null
      );
    });
+
+   User.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    User.belongsToMany(models.Book, {
+      through: "Checkout",
+      as: "User",
+      foreignKey: "userId",
+      otherKey: "bookId"
+    });
+  };
   return User;
 };
 //This is a fix by Samaila Philemon Bala in case you want to use ES6
