98c6ce954000192f6658d13568131fff78a7f26d
Michael Hegner <hegner123@gmail.com>
2019-11-25 12:28:29 -0500

    troubleshooting made changes to api routes and models/user.js

--- models/user.js
+++ models/user.js
@@ -13,6 +13,10 @@ module.exports = function(sequelize, DataTypes) {
         isEmail: true
       }
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     firstName: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -26,10 +30,6 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
           notEmpty: true
       }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
     },
     address: {
       type: DataTypes.STRING,
